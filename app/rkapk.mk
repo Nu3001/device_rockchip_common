70607f1c7e5e63a1d75cdd528076e0fc626422f6
Mike Wilson <mwilson@acesappraisal.com>
2015-03-07 15:51:25 -0500

    initial commit

---
+++ app/rkapk.mk
@@ -0,0 +1,6 @@
+CUR_PATH := device/rockchip/common/app
+PRODUCT_PACKAGES += \
+	MediaFloat \
+	RkExplorer \
+	RkVideoPlayer \
+        StressTest 
---
+++ etc/adblock.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+	device/rockchip/common/etc/.allBlock:system/etc/.allBlock \
+	device/rockchip/common/etc/.videoBlock:system/etc/.videoBlock \
+	device/rockchip/common/etc/librkbm.so:system/lib/librkbm.so \
+        device/rockchip/common/etc/bmi:system/bin/bmi \
+        device/rockchip/common/etc/bmd:system/bin/bmd \
