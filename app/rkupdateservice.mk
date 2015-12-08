f5df8bf29feb7041337cb85baba3e72f903f57fb
James Golovich <james@gnuinter.net>
2015-12-08 09:33:54 -0800

    Disable rkupdateservice
    TWRP doesn't support update.img files, if it ever does we can re-enable it.

--- app/rkupdateservice.mk
+++ app/rkupdateservice.mk
@@ -1,5 +1,7 @@
-PRODUCT_PACKAGES += \
-    RKUpdateService
+# Our Recovery (TWRP) doesn't support handling update.img files
+# If we ever do, we can re-enable this
+#PRODUCT_PACKAGES += \
+#    RKUpdateService
 
-    PRODUCT_COPY_FILES += \
-        device/rockchip/common/app/apk/RKUpdateService/librockchip_update_jni.so:system/lib/librockchip_update_jni.so
+#    PRODUCT_COPY_FILES += \
+#        device/rockchip/common/app/apk/RKUpdateService/librockchip_update_jni.so:system/lib/librockchip_update_jni.so
